2ba539e9f8a558ab895b169caea7cb0eb3be988d
Midori <chadifferus@gmail.com>
2019-04-27 10:41:11 -0300

    Changing IEnemy to iterface

--- LudumDare44/Assets/Scripts/Enemy/IEnemy.cs
+++ LudumDare44/Assets/Scripts/Enemy/IEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IEnemy : MonoBehaviour
+public interface IEnemy
 {
     // Start is called before the first frame update
     void Start()
