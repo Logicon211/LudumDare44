24b4f6eb1842173f15313ffe0a1d9d9ffbd1c79c
BroderickBeaman <broderick.beaman@unb.ca>
2019-04-27 11:49:01 -0300

    Updated power up abstract class

--- LudumDare44/Assets/Scripts/PowerUps/PowerUp.cs
+++ LudumDare44/Assets/Scripts/PowerUps/PowerUp.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PowerUp : MonoBehaviour
 {
+
+    public GameObject powerUpOverlay;
+
+    private bool pickupable;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickupable = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision Enter");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision With Player Enter");
+            pickupable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Collision Exit");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision With Player Exit");
+            pickupable = false;
+        }
+    }
+
+    public abstract void PickUp();
+
+    public bool IsPickupable()
+    {
+        return pickupable;
     }
 }
