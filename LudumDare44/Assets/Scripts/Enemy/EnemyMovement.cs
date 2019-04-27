eee9ca13e56b372868a156923b3274a806a1e9ec
Midori <chadifferus@gmail.com>
2019-04-27 11:41:56 -0300

    Setting up initial Ninja enemy and generic EnemyMovement script

--- LudumDare44/Assets/Scripts/Enemy/EnemyMovement.cs
+++ LudumDare44/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,46 +2,47 @@
 using System.Collections.Generic;
 using Enemy.Interface;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemyMovement : MonoBehaviour
 {
     public IEnemy controller;
     public float maxDistance = 20f;
-    public float shootDistance = 40f;
+    public float attackDistance = 40f;
     public float speed = 10f;
-    public float shootSpeed = 20f;
+    public float attackSpeed = 20f;
 
     private GameObject player;
     private Transform playerTransform;
     private bool move = false;
-    private bool shoot = false;
+    private bool attack = false;
     private Vector3 moveDir = Vector3.zero;
 
     // Use this for initialization
-    void Start () {
+    private void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 	
     // Update is called once per frame
-    void Update () {
+    private void Update () {
         float dist = Vector3.Distance(player.transform.position, transform.position);
         if (dist > maxDistance) {
             move = true;
         }
-        if (dist < shootDistance) {
-            shoot = true;
+        if (dist < attackDistance) {
+            attack = true;
         }
 
     }
 
-    void FixedUpdate() {
+    private void FixedUpdate() {
         Vector3 normal = (player.transform.position - transform.position).normalized;
         if (move) {
             moveDir = normal;	
         }
-        if (shoot) {
-            controller.Attack(normal.x * shootSpeed * Time.fixedDeltaTime, normal.y * shootSpeed * Time.fixedDeltaTime);
-            shoot = false;
+        if (attack) {
+            controller.Attack(normal.x * attackSpeed * Time.fixedDeltaTime, normal.y * attackSpeed * Time.fixedDeltaTime);
+            attack = false;
         }
         controller.Move(moveDir.x * speed * Time.fixedDeltaTime, moveDir.y * speed * Time.fixedDeltaTime);
         controller.Rotate(normal);
